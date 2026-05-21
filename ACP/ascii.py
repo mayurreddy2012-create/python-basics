5dd7cebc78c70e26757872f6a54f5ec8a54cb173
mayurreddy2012-create <mayur.reddy2012@gmail.com>
2026-05-21 12:17:39 +0530

    ASCII2

--- ACP/ascii.py
+++ ACP/ascii.py
@@ -1,3 +1,22 @@
-ch = input("Enter a character: ")
+ch = input("Enter exactly one character: ")
 
-print("ASCII value =", ord(ch))
+# Validate input length
+if len(ch) != 1:
+    print("Error: Please enter exactly one character.")
+else:
+    ascii_value = ord(ch)
+
+    print("ASCII value:", ascii_value)
+
+    # Check character type
+    if 'A' <= ch <= 'Z':
+        print("Type: Uppercase Letter")
+
+    elif 'a' <= ch <= 'z':
+        print("Type: Lowercase Letter")
+
+    elif '0' <= ch <= '9':
+        print("Type: Digit")
+
+    else:
+        print("Type: Special Character")
