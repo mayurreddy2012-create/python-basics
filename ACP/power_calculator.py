961186bc644160f3150e6b7830c8a566d36eac69
mayurreddy2012-create <mayur.reddy2012@gmail.com>
2026-05-31 11:19:39 +0530

    ACP

---
+++ ACP/power_calculator.py
@@ -0,0 +1,6 @@
+base = float(input("Enter the base number: "))
+exponent = float(input("Enter the exponent: "))
+
+result = pow(base, exponent)
+
+print("Result =", result)
---
+++ practice/loops/activity1.py
@@ -0,0 +1,8 @@
+s = input("enter a string: ")
+for i in s:
+    print(i)
+sum = 0
+n = int(input("enter a number: ")) 
+for i in range(1,n+1):
+       sum = sum + i
+print(sum)
